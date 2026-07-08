532cfbb66e1d8b471d68f75425486be14857a319
Myawwaa <murarikovajulia@gmail.com>
2026-07-08 13:38:17 +0200

    Add files via upload

---
+++ 10_program.py
@@ -0,0 +1,13 @@
+import time
+vek=int(input('kolik ti je broski?? '))
+if vek>= 18 :
+    print('no dobra tak jdi dal...')
+    time.sleep(5)
+    print('vlastne nejdes haha didopici')
+    time.sleep(4)
+    exit()
+else :
+    print('no tak di do pice prcku malej')
+    time.sleep(4)
+    exit()
+
